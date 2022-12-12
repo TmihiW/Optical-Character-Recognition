01b2519ebbc2e9e34fbdc042dd57cd604f2ebb8f
TmihiW <kadirfindik3871@gmail.com>
2022-12-12 13:04:11 +0300

    Initial commit

--- iP.py
+++ iP.py
@@ -3,20 +3,12 @@ from matplotlib import pyplot as plt
 #use gpu
 ocr_model = PaddleOCR(use_angle_cls=True,lang="tr")
 def rek(img):
-    try:    
-        #EasyOCR islemi
-        # ocr_model = PaddleOCR(use_angle_cls=True, lang="tr", use_gpu=True)
+    try:
         result= ocr_model.ocr(img, cls=True)
         if(result[0][1][0] == " "):
-            print("boş")
+            print("Empty")
         else:
             print(result[0][1][0])
-        # text = resultX[0][1][0]
-        # reader = easyocr.Reader(['en'])
-        # text = reader.readtext(Cropped)
-        # text = text[0][1]
-        # text = text.replace(" ", "")
-        # text = text.upper()
     except Exception:
         pass
-    return
+    return img
